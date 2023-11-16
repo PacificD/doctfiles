38271db173cf2c66434d22c7d32dc5adaca82ff5
PacificD <pacificd148@gmail.com>
2023-11-16 16:06:42 +0800

    remove aniamte plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
