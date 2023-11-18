c8ea769487091118f72607dd60cbf5d5f16b218a
PacificD <1484636442@qq.com>
2023-11-18 11:02:13 +0800

    Mason ensure installed

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,27 +1,28 @@
 return {
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "prettier")
-      opts.ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "css-lsp",
-        "cssmodules-language-server",
-        "json-lsp",
-        "lua-language-server",
-        "prisma-language-server",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "html-lsp",
-        "emmet-language-server",
-        "astro-language-server",
-        "svelte-language-server",
-        "vue-language-server",
-      }
-    end,
-  },
+	-- add any tools you want to have installed below
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = {
+				"stylua",
+				"shellcheck",
+				"shfmt",
+				"flake8",
+				"css-lsp",
+				"prettier",
+				"cssmodules-language-server",
+				"json-lsp",
+				"lua-language-server",
+				"prisma-language-server",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"html-lsp",
+				"emmet-language-server",
+				"yaml-language-server",
+				"astro-language-server",
+				"svelte-language-server",
+				"vue-language-server",
+			}
+		end,
+	},
 }
