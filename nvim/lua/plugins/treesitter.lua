2ea3679b8a13be5165dbeca77b31772af528aeb2
PacificD <1484636442@qq.com>
2023-11-18 11:04:26 +0800

    TS ensure insatlled

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,31 @@
 return {
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "swift",
-        "go",
-        "css",
-        "prisma",
-      },
-    },
-  },
+	-- add more treesitter parsers
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				"swift",
+				"go",
+				"css",
+				"prisma",
+				"vue",
+				"astro",
+				"svelte",
+			},
+		},
+	},
 }
