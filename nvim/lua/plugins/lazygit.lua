c3f0610a8ec2f9d3f9f1acccc6a5baae415d4c55
PacificD <pacificd148@gmail.com>
2023-11-23 12:41:25 +0800

    lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,10 +1,7 @@
--- nvim v0.8.0
-require("lazy").setup({
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+return {
+  "kdheepak/lazygit.nvim",
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
-})
+}
